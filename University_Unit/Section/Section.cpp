051f0c4cdff12a1cdf5b5990892eae35486100b2
Arash Zarghami <eng.ar.zarghami@gmail.com>
2024-01-01 10:26:42 +0330

    write isValid Id in class (Section) but i dont sure about it !

--- University_Unit/Section/Section.cpp
+++ University_Unit/Section/Section.cpp
@@ -2,3 +2,16 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
+Section::Section(string classId) {}
+
+bool Section::isClassIdValid(string classId) {
+    bool isValid = true;
+
+    if (classId == this->_classID) {
+        isValid = false;
+        return isValid;
+    }
+
+    return isValid;
+}
--- University_Unit/Section/Section.h
+++ University_Unit/Section/Section.h
@@ -1,27 +1,27 @@
-//#ifndef UNIVERSITY_UNIT_SECTION_H
-//#define UNIVERSITY_UNIT_SECTION_H
-//
-//#include <iostream>
-//#include <string>
-//#include <vector>
-//#include "../DateTime/Date.h"
-//#include "../Location/Location.h"
-//#include "../People/Person.h"
-//#include "../People/Student.h"
-//#include "../People/Teacher.h"
-//#include "../DateTime/Time.h"
-//
-//using namespace std;
-//
-//class Section {
-//private:
-//    string _classID;
-//    string _className;
-//    Time time;
-//    bool _videoProjector;
-//    Teacher teacher;
-//public:
-//};
-//
-//
-//#endif
+#ifndef UNIVERSITY_UNIT_SECTION_H
+#define UNIVERSITY_UNIT_SECTION_H
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../DateTime/Date.h"
+#include "../Location/Location.h"
+#include "../People/Person.h"
+#include "../People/Student.h"
+#include "../People/Teacher.h"
+#include "../DateTime/Time.h"
+
+using namespace std;
+
+class Section {
+private:
+    string _classID;
+public:
+    Section(string classId);
+
+    bool isClassIdValid(string classId);
+
+};
+
+
+#endif
